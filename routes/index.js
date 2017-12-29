4141187d8714d7a79edb17dd69c51f816a77e7a6
munetsi <zunguziramunetsi@gmail.com>
2017-12-29 23:58:55 +0200

    working api calls

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var request = require('request');
+var config = require('../config');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res,next) {
+
+  res.render('index',{"title": "Weather App"})
+
+})
 
 module.exports = router;
---
+++ routes/show.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var request = require('request');
+var config = require('../config');
+var router = express.Router();
+
+router.get('/', function(req, res,next) {
+   res.render('index')
+});
+
+router.post('/', function(req, res,next) {
+    var query     = req.body.query;
+
+    request('http://api.wunderground.com/api/'+config.WUNDERGROUND_API_KEY+'/conditions/q/'+query+'.json', function (error, response) {
+
+      var results  = JSON.parse(response.body);
+      if(results.current_observation){
+        var temp     =  results.current_observation.temp_c;
+        var icon     =  results.current_observation.icon_url;
+        var weather  =  results.current_observation.weather;
+        var location =  results.current_observation.display_location.full;
+
+
+        res.render('show',{"temp": temp, "icon": icon, "weather": weather,"location": location});
+      }else{
+        res.render("error", {"error": "Location not found.Please try anather location"})
+      }
+
+
+    })
+
+});
+
+module.exports = router;
